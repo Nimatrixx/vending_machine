03346e923e1c8c7c21a931d6a9795184155d3a05
nimatrixx <www.nimatrix@gmail.com>
2024-02-20 17:51:16 +0000

    add modules to vending machine

--- vending_machine.vhd
+++ vending_machine.vhd
@@ -14,6 +14,35 @@ end vending_machine;
 
 architecture Behavioral of vending_machine is
 	
+	component register_8 is
+		port ( input :in  std_logic_vector(7 downto 0) ;
+			load: in std_logic;
+			reset: in std_logic;
+			clk: in std_logic;
+			output: out std_logic_vector(7 downto 0)
+	 );
+	end component;
+
+	component mux4x1 is
+		 Port ( a, b, c, d : in  STD_LOGIC_VECTOR (7 downto 0);
+				  s : in  STD_LOGIC_VECTOR(1 downto 0);
+				  f : out  STD_LOGIC_VECTOR (7 downto 0));
+	end component;
+
+	component adder_subtractor_8 is
+		 Port ( A : in  STD_LOGIC_VECTOR (7 downto 0);
+				  B : in  STD_LOGIC_VECTOR (7 downto 0);
+				  OP : in STD_LOGIC;
+				  S : out  STD_LOGIC_VECTOR (7 downto 0);
+				  overflow : out  STD_LOGIC);
+	end component;
+
+	component subtractor_8 is
+		 Port ( a : in  STD_LOGIC_VECTOR (7 downto 0);
+					  b : in  STD_LOGIC_VECTOR (7 downto 0);
+					  s : out  STD_LOGIC_VECTOR (7 downto 0);
+					  c_out : out  STD_LOGIC);
+	end component;
 	
 begin
 	
