e97268777fbb8ed18cd5590926146f57ed9d50f2
nimatrixx <www.nimatrix@gmail.com>
2024-02-20 17:52:54 +0000

    combine modules with togather in vending machine

--- vending_machine.vhd
+++ vending_machine.vhd
@@ -44,7 +44,19 @@ architecture Behavioral of vending_machine is
 					  c_out : out  STD_LOGIC);
 	end component;
 	
+	signal addsub_coin1_out, addsub_coin2_out, sub_cost : STD_LOGIC_VECTOR(7 downto 0);
+	signal reg_in, balance : STD_LOGIC_VECTOR(7 downto 0);
+	signal cost  : STD_LOGIC_VECTOR(7 downto 0);
+	signal selected_ticket, s  : STD_LOGIC_VECTOR(1 downto 0);
+	
 begin
+
+	coin1_addsub : adder_subtractor_8 port map("00000101", balance, addsub_coin1_out);
+	coin2_addsub : adder_subtractor_8 port map("00001010", balance, addsub_coin2_out);
+	cost_mux : mux4x1 port map("00000101", "00001111", "00101000", "00000000", selected_ticket, cost);
+	cost_subtractor : subtractor_8 port map(balance, cost, sub_cost);
+	reg_mux : mux4x1 port map(balance, addsub_coin1_out, addsub_coin2_out, sub_cost, s, reg_in);
+	reg : register_8 port map(reg_in, '1', reset_balance, clk, balance);
 	
 end Behavioral;
 
